348324b7d4980c5bbb64cf7434c01dbd9114759e
Dragon-LZL <596495236@qq.com>
2022-01-25 22:56:19 +0800

    feat: current target complete

--- dist/index.js
+++ dist/index.js
@@ -49,6 +49,135 @@ const _ut = {
     // 获取数组随机一项
     arrRandomItem(arr) {
         return arr[Math.floor(Math.random() * arr.length)];
+    },
+    // 首字母大写
+    firstLetterUpper(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+    // 手机号脱敏
+    phoneDecode1(str) {
+        return `${str.slice(0, 3)}****${str.slice(7)}`;
+    },
+    phoneDecode2(str) {
+        const newStr = str.split("");
+        newStr.splice(3, 4, "****");
+        return newStr.join("");
+    },
+    // 下划线、驼峰互转
+    getCamelCase(str) {
+        return str.replace(/_([a-z])/g, (_, item) => {
+            return item.toUpperCase();
+        });
+    },
+    getUnderlineCase(str) {
+        return str.replace(/[A-Z]/g, item => {
+            return `_${item.toLowerCase()}`;
+        });
+    },
+    // 阻止冒泡
+    stopPropagation(e) {
+        e = e || window.event;
+        if (e.stopPropagation) {
+            // W3C阻止冒泡方法
+            e.stopPropagation();
+        }
+        else {
+            e.cancelBubble = true; // IE阻止冒泡方法
+        }
+    },
+    // 操作cookies
+    cookies: {
+        getItem: function (sKey) {
+            return (decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" +
+                encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") +
+                "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null);
+        },
+        setItem: function ({ sKey, sValue, vEnd, sPath, sDomain, bSecure }) {
+            if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) {
+                return false;
+            }
+            var sExpires = "";
+            if (vEnd) {
+                switch (vEnd.constructor) {
+                    case Number:
+                        sExpires =
+                            vEnd === Infinity
+                                ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+                                : "; max-age=" + vEnd;
+                        break;
+                    case String:
+                        sExpires = "; expires=" + vEnd;
+                        break;
+                    case Date:
+                        sExpires = "; expires=" + vEnd.toUTCString();
+                        break;
+                }
+            }
+            document.cookie =
+                encodeURIComponent(sKey) +
+                    "=" +
+                    encodeURIComponent(sValue) +
+                    sExpires +
+                    (sDomain ? "; domain=" + sDomain : "") +
+                    (sPath ? "; path=" + sPath : "") +
+                    (bSecure ? "; secure" : "");
+            return true;
+        },
+        removeItem: function (sKey, sPath, sDomain) {
+            if (!sKey || !this.hasItem(sKey)) {
+                return false;
+            }
+            document.cookie =
+                encodeURIComponent(sKey) +
+                    "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
+                    (sDomain ? "; domain=" + sDomain : "") +
+                    (sPath ? "; path=" + sPath : "");
+            return true;
+        },
+        hasItem: function (sKey) {
+            return new RegExp("(?:^|;\\s*)" +
+                encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") +
+                "\\s*\\=").test(document.cookie);
+        },
+        keys: function () {
+            var aKeys = document.cookie
+                .replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "")
+                .split(/\s*(?:\=[^;]*)?;\s*/);
+            for (var nIdx = 0; nIdx < aKeys.length; nIdx++) {
+                aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]);
+            }
+            return aKeys;
+        }
+    },
+    // 设备判断
+    device: {
+        isMobile() {
+            if (navigator.userAgent.match(/(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i)) {
+                return "mobile";
+            }
+            return "desktop";
+        },
+        isAppleMobileDevice() {
+            let reg = /iphone|ipod|ipad|Macintosh/i;
+            return reg.test(navigator.userAgent.toLowerCase());
+        },
+        isAndroidMobileDevice() {
+            return /android/i.test(navigator.userAgent.toLowerCase());
+        },
+        osType() {
+            const agent = navigator.userAgent.toLowerCase();
+            const isMac = /macintosh|mac os x/i.test(navigator.userAgent);
+            const isWindows = agent.indexOf("win64") >= 0 ||
+                agent.indexOf("wow64") >= 0 ||
+                agent.indexOf("win32") >= 0 ||
+                agent.indexOf("wow32") >= 0;
+            if (isWindows) {
+                return "windows";
+            }
+            if (isMac) {
+                return "mac";
+            }
+        }
     }
 };
 export default _ut;
--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,171 @@ const _ut: any = {
 
   // 获取数组随机一项
   arrRandomItem<T>(arr: T[]): T {
-    return arr[Math.floor(Math.random() * arr.length)]
+    return arr[Math.floor(Math.random() * arr.length)];
+  },
+
+  // 首字母大写
+  firstLetterUpper(str: string) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  },
+
+  // 手机号脱敏
+  phoneDecode1(str: string) {
+    return `${str.slice(0, 3)}****${str.slice(7)}`;
+  },
+  phoneDecode2(str: string) {
+    const newStr = str.split("");
+    newStr.splice(3, 4, "****");
+    return newStr.join("");
+  },
+
+  // 下划线、驼峰互转
+  getCamelCase(str: string) {
+    return str.replace(/_([a-z])/g, (_, item) => {
+      return item.toUpperCase();
+    });
+  },
+  getUnderlineCase(str: string) {
+    return str.replace(/[A-Z]/g, item => {
+      return `_${item.toLowerCase()}`;
+    });
+  },
+
+  // 阻止冒泡
+  stopPropagation(e: Event) {
+    e = e || window.event;
+    if (e.stopPropagation) {
+      // W3C阻止冒泡方法
+      e.stopPropagation();
+    } else {
+      e.cancelBubble = true; // IE阻止冒泡方法
+    }
+  },
+
+  // 操作cookies
+  cookies: {
+    getItem: function(sKey: string) {
+      return (
+        decodeURIComponent(
+          document.cookie.replace(
+            new RegExp(
+              "(?:(?:^|.*;)\\s*" +
+                encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") +
+                "\\s*\\=\\s*([^;]*).*$)|^.*$"
+            ),
+            "$1"
+          )
+        ) || null
+      );
+    },
+    setItem: function({
+      sKey,
+      sValue,
+      vEnd,
+      sPath,
+      sDomain,
+      bSecure
+    }: {
+      sKey: string;
+      sValue: string;
+      vEnd?: number | string | Date;
+      sPath?: string;
+      sDomain?: string;
+      bSecure?: string;
+    }) {
+      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) {
+        return false;
+      }
+      var sExpires = "";
+      if (vEnd) {
+        switch (vEnd.constructor) {
+          case Number:
+            sExpires =
+              vEnd === Infinity
+                ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+                : "; max-age=" + vEnd;
+            break;
+          case String:
+            sExpires = "; expires=" + vEnd;
+            break;
+          case Date:
+            sExpires = "; expires=" + (vEnd as Date).toUTCString();
+            break;
+        }
+      }
+      document.cookie =
+        encodeURIComponent(sKey) +
+        "=" +
+        encodeURIComponent(sValue) +
+        sExpires +
+        (sDomain ? "; domain=" + sDomain : "") +
+        (sPath ? "; path=" + sPath : "") +
+        (bSecure ? "; secure" : "");
+      return true;
+    },
+    removeItem: function(sKey: string, sPath?: string, sDomain?: string) {
+      if (!sKey || !this.hasItem(sKey)) {
+        return false;
+      }
+      document.cookie =
+        encodeURIComponent(sKey) +
+        "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
+        (sDomain ? "; domain=" + sDomain : "") +
+        (sPath ? "; path=" + sPath : "");
+      return true;
+    },
+    hasItem: function(sKey: string) {
+      return new RegExp(
+        "(?:^|;\\s*)" +
+          encodeURIComponent(sKey).replace(/[-.+*]/g, "\\$&") +
+          "\\s*\\="
+      ).test(document.cookie);
+    },
+    keys: function() {
+      var aKeys = document.cookie
+        .replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "")
+        .split(/\s*(?:\=[^;]*)?;\s*/);
+      for (var nIdx = 0; nIdx < aKeys.length; nIdx++) {
+        aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]);
+      }
+      return aKeys;
+    }
+  },
+
+  // 设备判断
+  device: {
+    isMobile() {
+      if (
+        navigator.userAgent.match(
+          /(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i
+        )
+      ) {
+        return "mobile";
+      }
+      return "desktop";
+    },
+    isAppleMobileDevice() {
+      let reg = /iphone|ipod|ipad|Macintosh/i;
+      return reg.test(navigator.userAgent.toLowerCase());
+    },
+    isAndroidMobileDevice() {
+      return /android/i.test(navigator.userAgent.toLowerCase());
+    },
+    osType() {
+      const agent = navigator.userAgent.toLowerCase();
+      const isMac = /macintosh|mac os x/i.test(navigator.userAgent);
+      const isWindows =
+        agent.indexOf("win64") >= 0 ||
+        agent.indexOf("wow64") >= 0 ||
+        agent.indexOf("win32") >= 0 ||
+        agent.indexOf("wow32") >= 0;
+      if (isWindows) {
+        return "windows";
+      }
+      if (isMac) {
+        return "mac";
+      }
+    }
   }
 };
 
